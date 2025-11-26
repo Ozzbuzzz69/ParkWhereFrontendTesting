6ace33b99157a8e6580891762131db29166a610f
Nikolaj04 <felsdd04@gmail.com>
2025-11-26 15:23:40 +0100

    homeSide

--- homeScript.js
+++ homeScript.js
@@ -3,11 +3,23 @@ const baseUrl = "indsæt api"
 Vue.createApp({
     data(){
         return{
-            freeParkingSpots: null
+            parkingSpotAmountWest: 97,
+            parkingSpotAmountSouth: 22,
+            parkingSpotAmountNorth: 140,
+
+            latestUpdate: "14:30"
         }
     },
 
-    method:{
-
+    methods:{
+        async getParkingSpotAmount(){
+            try {
+                response = await axios.get(baseUrl)
+                this.parkingSpotAmount = response.data //skal rettes når api er klar
+            }
+            catch {
+                alert(ex.message)
+            }
+        },
     }
 }).mount("#app")
